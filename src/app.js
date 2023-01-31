6ef90bbfda22c2ea68bed2bc6a022aec2ff5ad18
Daniel Abdel <danielmalek101@gmail.com>
2023-01-31 12:28:55 -0300

    chore: config deploy on render

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log("server online, on port " + process.env.PORT);
+});
